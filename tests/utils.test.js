eab29408e3245d4de59007bada10464be7f32e78
APaud3l <aamod.paudel@coderacademy.edu.au>
2026-04-13 19:53:16 +1000

    feat: Added another test case to check if the added numbers are not zero

--- tests/utils.test.js
+++ tests/utils.test.js
@@ -1,7 +1,13 @@
 const { sum } = require("../src/utils");
 
 // test('describe', function())
+// it('describe', function())
 
-test('adds two numbers', () => {
+it('adds two numbers', () => {
     expect(sum(2, 3)).toBe(5);
+});
+
+// it.only('checks if the two added numbers are not 0', () => {
+it('checks if the two added numbers are not 0', () => {
+    expect(sum(2, 3)).not.toBe(0);
 });
