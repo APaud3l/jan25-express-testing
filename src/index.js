043a42b81afacc7da9d7a76863f8ed9a380268b6
APaud3l <aamod.paudel@coderacademy.edu.au>
2026-04-13 20:41:10 +1000

    feat: Looked at a few jest running options

--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
 const express = require('express');
+const { getTasks, addTask } = require('./utils');
 
 const app = express();
 
 app.use(express.json()); // Parse JSON bodies
+
+app.get('/tasks', (request, response) => {
+    response.json({
+        tasks: getTasks()
+    });
+});
+
+app.post('/tasks', (request, reponse) => {
+    const task = addTask(request.body);
+    response.status(201).json(task);
+});
 
 module.exports = app;
